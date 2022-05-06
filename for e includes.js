124950622e0619d7eb695a31442a4998ae2cb5f9
djimenez25 <daniel.jimenez@bootcamp-upgrade.com>
2022-05-06 12:47:06 +0200

    Iteration#1

---
+++ for e includes.js
@@ -0,0 +1,26 @@
+console.log("Archivo for e includes funcionando OK")
+
+// Dado el siguiente javascript usa forof para recorrer el array de películas, genera un nuevo array con las categorías de las películas e imprime por consola el array de categorías. Ten en cuenta que las categorías no deberían repetirse. Para filtrar las categorías puedes ayudarte de la función .includes()
+
+const movies = [
+    {title: 'Madaraspar', duration: 192, categories: ['comedia', 'aventura']},
+    {title: 'Spiderpan', duration: 122, categories: ['aventura', 'acción']},
+    {title: 'Solo en Whatsapp', duration: 223, categories: ['comedia', 'thriller']},
+    {title: 'El gato con guantes', duration: 111, categories: ['comedia', 'aventura', 'animación']},
+];
+
+
+const moviesCategories = []
+
+
+
+for (const movie of movies) {
+    console.log(movie)
+    for (const category of movie.categories) {
+        if(moviesCategories.indexOf(category) ===-1) {
+            moviesCategories.push(category)
+        }
+    }
+}
+
+console.log(moviesCategories)
